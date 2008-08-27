8b68b64c44a3aa9a47a5038684cae66937796844
Juan Lupion <juan.lupion@the-cocktail.com>
2008-08-27 20:26:53 +0200

    Fixed paginated posts...

--- app/controllers/front_pages_controller.rb
+++ app/controllers/front_pages_controller.rb
@@ -5,7 +5,7 @@ class FrontPagesController < ApplicationController
   def index
     page = params[:page]
     @posts = FeedPost.paginate(
-      :page => page, :order => 'date_published DESC', :per_page => 5)
+      :page => page, :order => 'date_published DESC', :per_page => 20)
       
     respond_to do |format|
       format.html
